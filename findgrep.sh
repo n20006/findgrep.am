0c8cbd19bbfd6997b17df497277656fd386ae984
kinjyo Takami <n20006@std.it-college.ac.jp>
2020-06-24 11:53:38 +0900

    file pattern

--- findgrep.sh
+++ findgrep.sh
@@ -2,7 +2,13 @@
 
 pattern=$1
 directory=$2
+name=$3
 if [ -z "$directory" ]; then
 	directory='.'
 fi
-find "directory" -type f | xargs grep -nH "$pattern"
+if [ -z "$name" ]; then
+	name='*'
+fi
+# -n : print line number
+# -H : print the file name
+find "directory" -type f -name "$name" | xargs grep -nH "$pattern"
